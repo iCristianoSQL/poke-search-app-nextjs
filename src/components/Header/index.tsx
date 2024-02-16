db2aab9d2e23d98376b9a91528a0885e01a48b57
Cristiano <cristianosql02@gmail.com>
2024-02-16 19:24:42 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="w-full h-11 bg-[#FFF] flex justify-center items-center">
+            <h1>Aqui fica a Header</h1>
+        </header>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        darkPurple: "#260e3f",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
