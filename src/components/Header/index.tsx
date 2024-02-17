451d5ac0ebe02a87db51b77e2714bd15c1e1d1de
Cristiano <cristianosql02@gmail.com>
2024-02-16 22:24:24 -0300

    feat: v1 estruturação da header e adição das imagens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import TextInput from "../Inputs/Text";
+
 export const Header = () => {
-    return (
-        <header className="w-full h-11 bg-[#FFF] flex justify-center items-center">
-            <h1>Aqui fica a Header</h1>
-        </header>
-    );
-}
+  return (
+    <header className="w-full h-16 bg-transparent flex justify-between items-center border-b border-white border-solid">
+      <Image src="/react-logo.png" width={64} height={64} alt="Logo" />
+      <TextInput width="100vw" placeholder="Busque por um Pokemon" />
+      <Image
+        src="/github-icon.png"
+        width={64}
+        height={64}
+        className="mr-2"
+        alt="Github Icon"
+      />
+    </header>
+  );
+};
