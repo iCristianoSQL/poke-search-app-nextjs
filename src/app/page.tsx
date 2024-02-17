2f9de4d37a830decce7f36133a5c82b90e95bc6a
Cristiano <cristianosql02@gmail.com>
2024-02-17 00:25:27 -0300

    chore: adicionando novos componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Body</h1>
+    <main className="h-full w-full flex flex-col gap-4 items-center">
+      <section className="bg-black w-4/5">Pokedex</section>
     </main>
   );
 }
