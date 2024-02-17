ab49f16549f5041cca9331760f6b8184b9f5280e
Cristiano <cristianosql02@gmail.com>
2024-02-17 19:34:34 -0300

    feat: criação dos cards de pokemon

---
+++ src/components/PokeCard/index.tsx
@@ -0,0 +1,77 @@
+import { useText } from "@/contexts/SearchContext";
+import { usePokemons } from "@/services/pokemon/pokemon";
+import { strings } from "@/services/utils/methods";
+import Image from "next/image";
+import { IPokeCard } from "./types";
+import { Button } from "../Button";
+import SelectInput from "../Inputs/Select";
+import { useForm } from "react-hook-form";
+import { pageSchema } from "@/services/utils/schemas";
+import { z } from "zod";
+
+type TFilterOptions = z.infer<typeof pageSchema>;
+
+export const PokeCard = ({ pageController }: IPokeCard): JSX.Element => {
+  const { register, watch } = useForm<TFilterOptions>();
+
+  const { pokemons } = usePokemons(pageController);
+  const { text, setText } = useText();
+  const azValue = watch("aZSelect");
+  const idValue = watch("iDSelect");
+
+  const filteredAndSortedPokemons = pokemons
+    ?.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(text.toLowerCase())
+    )
+    ?.sort((a, b) => {
+      return Number(azValue) === 1
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name);
+    })
+    .sort((a, b) => {
+      return Number(idValue) === 1 ? a.id - b.id : b.id - a.id;
+    });
+
+  const clearFilters = () => {
+    setText("");
+  };
+
+  return (
+    <>
+      <nav className="gap-4 w-4/5 flex justify-end	mt-8">
+        <Button handleClick={clearFilters} label="Limpar filtros" />
+        <SelectInput
+          label="A-Z"
+          register={register("aZSelect")}
+          options={[
+            { value: 1, label: "A-Z" },
+            { value: 2, label: "Z-A" },
+          ]}
+        />
+        <SelectInput
+          label="ID"
+          register={register("iDSelect")}
+          options={[
+            { value: 1, label: "Menor" },
+            { value: 2, label: "Maior" },
+          ]}
+        />
+      </nav>
+      {filteredAndSortedPokemons?.map((pokemon) => (
+        <div
+          key={pokemon.id}
+          className="w-56 items-center flex flex-col rounded-lg border border-white border-solid"
+        >
+          <p>{pokemon.id}</p>
+          <Image
+            src={pokemon.image}
+            alt={pokemon.name}
+            width={100}
+            height={100}
+          />
+          <p className="font-semibold">{strings.capitalize(pokemon.name)}</p>
+        </div>
+      ))}
+    </>
+  );
+};
